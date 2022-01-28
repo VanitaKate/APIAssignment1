2f849613f9b6453a889234ff7dfc0221bb2c3a71
VanitaKate <58419931+VanitaKate@users.noreply.github.com>
2022-01-28 12:04:43 +0530

    Update CreateUserPostRequest.java

--- Assignment1/src/test/java/APIRequestValidations/CreateUserPostRequest.java
+++ Assignment1/src/test/java/APIRequestValidations/CreateUserPostRequest.java
@@ -2,12 +2,11 @@ package APIRequestValidations;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import POJOs.PayloadPOJO;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class CreateUserPostRequest {
+public class CreateUserPostRequest extends Report{
 	@Test	
 	public void CreateUser() {
 		PayloadPOJO payload=new PayloadPOJO("Vanita", "leader");
@@ -17,10 +16,10 @@ public class CreateUserPostRequest {
 				.accept("application/json")
 				.body(payload).when().post("/api/users").then().statusCode(201).extract().response();
 
-		System.out.println("String Reponse=: " +response.asString());
+		System.out.println("String Reponse: " +response.asString());
 		
 //		JSONObject json=new JSONObject(response);
-		System.out.println(response.jsonPath().get("name"));
+		System.out.println(response.jsonPath().get("name").toString());
 		Assert.assertEquals(response.jsonPath().get("name"), "Vanita", "User did not get added");
 
 	}
